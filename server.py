66ab7e3b5c8cd4afea9d377c2a6d6733ae47d349
abdullahferozonline <119345525+abdullahferozonline@users.noreply.github.com>
2026-04-11 11:52:57 +0530

    Use environment variable for server port

--- server.py
+++ server.py
@@ -36,5 +36,8 @@ def answer_sdp(data):
 def ice(data):
     emit('ice_candidate', data, room=users[data['to']])
 
+import os
+
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
